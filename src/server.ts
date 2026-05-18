aca85b5a460d51940b909557347ea350abd58814
Galaceon <antoniogarcialtr@gmail.com>
2026-05-18 22:45:19 +0200

    connecting express app  with db and applying env var

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,20 @@
 import express from 'express'
 import router from './router'
+import db from './config/db'
+
+// Conectar a DB
+async function connectDB() {
+    try {
+        await db.authenticate()
+        db.sync()
+        console.log('conexion exitosa a la db')
+    } catch(error) {
+        console.log(error)
+        console.log('Hubo un error al conectarse a la db')
+    }
+}
+
+connectDB()
 
 const server = express()
 
