6e55c217a8a276d2ef0545cf18b547b5884e30fc
Galaceon <antoniogarcialtr@gmail.com>
2026-05-21 00:31:20 +0200

    validation to create product in server

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,7 +1,25 @@
 import { Request, Response } from 'express'
+import { check, validationResult } from 'express-validator'
 import Product from '../models/Product.model'
 
 export const createProduct = async (req : Request, res: Response) => {
+
+    // Validación 
+    await check('name')
+        .notEmpty().withMessage('El nombre del producto no puede ir vacio')
+        .run(req)
+    await check('price')
+        .isNumeric().withMessage('Valor no Valido')
+        .notEmpty().withMessage('El precio del producto no puede ir vacio')
+        .custom(value => value > 0).withMessage('Precio no valido')
+        .run(req)
+
+    let errors = validationResult(req)
+
+    if(!errors.isEmpty()) {
+
+        return res.status(400).json({errors: errors.array()})
+    }
 
     const product = await Product.create(req.body)
 
