f2622f102a604183b5a20403272dbe1c55e656f1
Galaceon <antoniogarcialtr@gmail.com>
2026-05-20 23:59:58 +0200

    adding products to db

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from 'express'
+import Product from '../models/Product.model'
 
-export const createProduct = (req : Request, res: Response) => {
+export const createProduct = async (req : Request, res: Response) => {
+
+    const product = new Product(req.body)
+    product.save()
 
     res.json('Desde Post')
 }
