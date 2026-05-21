5a0f48e76fc74ba29fdbbcb2204b32cc682edc9a
Galaceon <antoniogarcialtr@gmail.com>
2026-05-22 00:39:35 +0200

    creating endpoint to get all products

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,8 +1,12 @@
+import { Request, Response, NextFunction } from 'express'
+import { validationResult } from 'express-validator'
 
+export const handleInputErrors = (req: Request, res: Response, next: NextFunction) => {
 
-export const handleInputErrors = (req, res, next) => {
+    let errors = validationResult(req)
+    if(!errors.isEmpty()) {
 
-    console.log('desde middleware...')
-
+        return res.status(400).json({errors: errors.array()})
+    }
     next()
 }
