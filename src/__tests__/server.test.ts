3d759d10951502b3b4b96598a252beccc5bf1121
Galaceon <antoniogarcialtr@gmail.com>
2026-05-27 00:11:32 +0200

    finishing trying some tests

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -5,6 +5,10 @@ describe('GET /api', () => {
     it('should send back a json response', async () => {
         const res = await supertest(server).get('/api')
 
-        console.log(res)
+        expect(res.status).toBe(200)
+        expect(res.header['content-type']).toMatch(/json/)
+        expect(res.body.msg).toBe('Desde API')
+
+        console.log(res.body.msg)
     })
 })
