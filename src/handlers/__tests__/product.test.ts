65dd072d863979af7285fac33fbcdc07523deda4
Galaceon <antoniogarcialtr@gmail.com>
2026-05-28 19:11:46 +0200

    test for create produc validation

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -11,6 +11,17 @@ describe('POST /api/products', () => {
         await db.close()
     })
 
+    it('should display validation errors', async () => {
+        const response = await supertest(server).post('/api/products').send({})
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(4)
+
+        expect(response.status).toBe(404)
+        expect(response.body.errors).toHaveLength(2)
+    })
+
     it('should create a new product', async () => {
         const response = await supertest(server).post('/api/products').send({
             name : "Taza Gaming",
@@ -22,6 +33,7 @@ describe('POST /api/products', () => {
 
         expect(response.status).not.toBe(404)
         expect(response.status).not.toBe(200)
+        expect(response.body).not.toHaveProperty('error')
 
         console.log(response.status)
     })
