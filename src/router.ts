ca659b60eeb9240c1a66cc8107f45cc020d48fce
Galaceon <antoniogarcialtr@gmail.com>
2026-05-30 21:58:22 +0200

    documenting products and their atribbutes

--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,32 @@ import { handleInputErrors } from './middleware'
 
 const router = Router()
 
+/**
+ * @swagger
+ * components:
+ *      schemas:
+ *          Product:
+ *              type: object
+ *              properties:
+ *                  id:
+ *                      type: integer
+ *                      description: The Product ID
+ *                      example: 1
+ *                  name:
+ *                      type: string
+ *                      description: The Product Name
+ *                      example: Raton Gaming LG
+ *                  price:
+ *                      type: number
+ *                      description: The Product Price
+ *                      example: 250
+ *                  availability:
+ *                      type: boolean
+ *                      description: The Product Availability
+ *                      example: true
+ * 
+ */
+
 //Routing
 router.get('/', getProducts)
 router.get('/:id', 
